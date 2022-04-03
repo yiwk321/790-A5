fa03e442a758fd77c103b17e77137a6c981e2bbe
Ken Liu <yiwk321@live.unc.edu>
2022-04-03 12:01:18 -0400

    force will point to the raycast hitpoint, instead of center of the object

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class Interactable : MonoBehaviour
-{
+public class Interactable : MonoBehaviour {
     private bool hovering = false;
     private bool selected = false;
     public Material hoveringMaterial = null;
     public Material selectedMaterial = null;
-    public Material normalMaterial = null;
+    private Material defaultMaterial = null;
     public float threshold = 100;
     private float force = 10;
     public XRRig player = null;
@@ -18,28 +17,35 @@ public class Interactable : MonoBehaviour
     private Rigidbody rigidBody = null;
     public int multiplier = 1;
     private float mass = 0;
-    private UnityEngine.XR.InputDevice leftController; 
-    private UnityEngine.XR.InputDevice rightController; 
-    private bool leftCurrentlySelected = false; 
-    private bool rightCurrentlySelected = false; 
-    void Start()
-    {
+    private UnityEngine.XR.InputDevice leftController;
+    private UnityEngine.XR.InputDevice rightController;
+    // private bool leftCurrentlySelected = false;
+    // private bool rightCurrentlySelected = false;
+    private XRRayInteractor leftRayInteractor = null;
+    private XRRayInteractor rightRayInteractor = null;
+
+    void Start() {
         meshRenderer = GetComponent<MeshRenderer>();
-        meshRenderer.material = normalMaterial;
+        defaultMaterial = meshRenderer.material;
         rigidBody = GetComponent<Rigidbody>();
         mass = rigidBody.mass;
+        player = FindObjectOfType<XRRig>();
 
         var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
 
-        
+
         var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
 
-        leftController = leftHandDevices[0];
-        rightController = rightHandDevices[0];
+
+        leftRayInteractor = GameObject.Find("LeftHand Controller").GetComponent<XRRayInteractor>();
+        rightRayInteractor = GameObject.Find("RightHand Controller").GetComponent<XRRayInteractor>();
 
         force = FindObjectOfType<Avatar>().force;
+
+        leftController = leftHandDevices[0];
+        rightController = rightHandDevices[0];
     }
 
     public void Hover() {
@@ -51,48 +57,50 @@ public class Interactable : MonoBehaviour
         hovering = false;
         changeMaterial();
     }
-    
-    public void Select() {
-        bool leftTriggerValue = false; 
-        bool rightTriggerValue = false;
 
-        if (leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out leftTriggerValue) && leftTriggerValue)
-        {
+    public void Select() {
+        bool leftTriggerValue = false;
+        bool rightTriggerValue = false;
+        RaycastHit hit;
+        if (leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out leftTriggerValue) && leftTriggerValue) {
             selected = true;
             // From: https://docs.unity3d.com/ScriptReference/GameObject-transform.html
-            Vector3 direction = gameObject.transform.position - player.transform.position;
-            direction.Normalize();
-            if (mass > threshold) {
-                player.GetComponent<Rigidbody>().AddForce(direction * force * multiplier, ForceMode.Impulse);
-            } else {
-                rigidBody.AddForce(-1* force * direction * multiplier, ForceMode.Impulse);
+            if (leftRayInteractor.TryGetCurrent3DRaycastHit(out hit)) {
+                Vector3 direction = hit.point - player.transform.position;
+                direction.Normalize();
+                if (mass > threshold) {
+                    player.GetComponent<Rigidbody>().AddForce(direction * force * multiplier, ForceMode.Impulse);
+                } else {
+                    rigidBody.AddForce(-1 * force * direction * multiplier, ForceMode.Impulse);
+                }
             }
             changeMaterial();
             Debug.Log("Left Trigger button is pressed.");
-        } else if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out rightTriggerValue) && rightTriggerValue)
-        {
+        } else if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out rightTriggerValue) && rightTriggerValue) {
             selected = true;
             // From: https://docs.unity3d.com/ScriptReference/GameObject-transform.html
-            Vector3 direction = gameObject.transform.position - player.transform.position;
-            direction.Normalize();
-            if (mass > threshold) {
-                player.GetComponent<Rigidbody>().AddForce(-1 * direction * force * multiplier, ForceMode.Impulse);
-            } else {
-                rigidBody.AddForce(force * direction * multiplier, ForceMode.Impulse);
+            if (rightRayInteractor.TryGetCurrent3DRaycastHit(out hit)) {
+                Vector3 direction = hit.point - player.transform.position;
+                direction.Normalize();
+                if (mass > threshold) {
+                    player.GetComponent<Rigidbody>().AddForce(-1 * direction * force * multiplier, ForceMode.Impulse);
+                } else {
+                    rigidBody.AddForce(force * direction * multiplier, ForceMode.Impulse);
+                }
             }
             changeMaterial();
             Debug.Log("Right Trigger button is pressed.");
-        } 
+        }
     }
-    
+
     public void ExitSelected() {
         selected = false;
         changeMaterial();
     }
-    
-    private void changeMaterial(){
+
+    private void changeMaterial() {
         if (selected) meshRenderer.material = selectedMaterial;
         else if (hovering) meshRenderer.material = hoveringMaterial;
-        else meshRenderer.material = normalMaterial;
+        else meshRenderer.material = defaultMaterial;
     }
 }
